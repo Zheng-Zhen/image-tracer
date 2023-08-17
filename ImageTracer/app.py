b1b50f6a30a1c9d739f3fd590042286c8c74385e
Shaun <89009401+ShaunZhxiong@users.noreply.github.com>
2023-08-17 00:10:50 -0500

    feat: ingest coordinate list for the scenario of multiple points submitted

--- ImageTracer/app.py
+++ ImageTracer/app.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 import requests
 from typing import Optional
+import os
 
 app = FastAPI()
 
-MAPBOX_TOKEN = "YOUR_MAPBOX_ACCESS_TOKEN"
+MAPBOX_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
 
 
 @app.get("/directions")
-def get_directions(start: Optional[str] = None, end: Optional[str] = None):
-    url = f"https://api.mapbox.com/directions/v5/mapbox/walking/{start};{end}?access_token={MAPBOX_TOKEN}"
+def get_directions(coordinate_list: Optional[list] = None):
+    if coordinate_list is None:
+        return {"error": "No coordinates provided"}
+    else:
+        coordinates = ";".join([f"{c[0]},{c[1]}" for c in coordinate_list])
+
+    url = f"https://api.mapbox.com/directions/v5/mapbox/walking/{coordinates}?access_token={MAPBOX_TOKEN}"
 
     try:
         response = requests.get(url)
