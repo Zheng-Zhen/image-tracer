d970adf2fb3c62b3682a005bae020027d0f1ca88
zhen <xzzxzz2010@163.com>
2023-08-27 16:03:32 +0800

    feat: 新增mapbox地图，token隐藏在后端

--- ImageTracer/app.py
+++ ImageTracer/app.py
@@ -1,11 +1,31 @@
-from fastapi import FastAPI
-import requests
-from typing import Optional
 import os
+from typing import Optional
 
+import httpx # for proxying mapbox tiles
+import dotenv
+import requests
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+
+# get env variables
+env_path = os.path.join(os.path.dirname(__file__), "conf", ".env")
+dotenv.load_dotenv(dotenv_path=env_path)
+MAPBOX_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
+
+# set up app
 app = FastAPI()
 
-MAPBOX_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
+# set up CORS
+origins = [
+    "http://localhost:5500",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/directions")
@@ -26,3 +46,28 @@ def get_directions(coordinate_list: Optional[list] = None):
     except Exception as e:
         print(e)
         return {"error": "An error occurred"}
+
+@app.get("/map/{path:path}")
+async def mapbox_proxy(request: Request):
+    # redirect to mapbox api, with token
+    mapbox_base_url = f"https://api.mapbox.com/"
+    forwarded_headers = {
+        header: value for header, value in request.headers.items() if header.lower() != "host"
+    }
+    # add token to params
+    # create new params dict
+    params = request.query_params._dict
+    params["access_token"] = MAPBOX_TOKEN
+    # get endpoint
+    url = mapbox_base_url + request.path_params["path"]
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url,
+            params=params,
+            headers={"Authorization": f"Bearer {MAPBOX_TOKEN}", **forwarded_headers},
+        )
+        return Response(content=response.content, media_type=response.headers.get("content-type", None))
+
+    # TODO: add token verification
+
--- web/js/draw.js
+++ web/js/draw.js
@@ -1,14 +1,17 @@
+mapboxgl.config.API_URL = 'http://localhost:23333/map';
+
 function initializeMap() {
     const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-122.662323, 45.523751], // starting position
-      zoom: 12
+        accessToken: "123",
+        container: 'map',
+        style: 'mapbox://styles/zzxzzxz/cllsxjwel004j01rf2un96dda',
+        center: [-122.662323, 45.523751], // starting position
+        zoom: 12
     });
-  
+
     const bounds = [
-      [-123.069003, 45.395273],
-      [-122.303707, 45.612333]
+        [-123.069003, 45.395273],
+        [-122.303707, 45.612333]
     ];
 
     map.setMaxBounds(bounds);
@@ -20,24 +23,24 @@ function addAnchortoMap(waypoint) {
         id: 'point',
         type: 'circle',
         source: {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'Point',
-                  coordinates: waypoint
-                }
-              }
-            ]
-          }
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'Point',
+                            coordinates: waypoint
+                        }
+                    }
+                ]
+            }
         },
         paint: {
-          'circle-radius': 10,
-          'circle-color': '#3887be'
+            'circle-radius': 10,
+            'circle-color': '#3887be'
         }
     });
 }
@@ -45,9 +48,9 @@ function addAnchortoMap(waypoint) {
 
 function addAnchorsFromList(coordinate_list) {
     coordinate_list.forEach(coord => {
-      addAnchortoMap(coord);
+        addAnchortoMap(coord);
     });
-  }
+}
 
 
 function addRouteToMap(returned_route) {
@@ -55,8 +58,8 @@ function addRouteToMap(returned_route) {
         type: 'Feature',
         properties: {},
         geometry: {
-          type: 'LineString',
-          coordinates: returned_route
+            type: 'LineString',
+            coordinates: returned_route
         }
     };
 
@@ -67,24 +70,28 @@ function addRouteToMap(returned_route) {
     // otherwise, we'll make a new request
     else {
         map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-            type: 'geojson',
-            data: geojson
-        },
-        layout: {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        paint: {
-            'line-color': '#3887be',
-            'line-width': 5,
-            'line-opacity': 0.75
-        }
+            id: 'route',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: geojson
+            },
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round'
+            },
+            paint: {
+                'line-color': '#3887be',
+                'line-width': 5,
+                'line-opacity': 0.75
+            }
         });
     }
 }
 
 
 
+
+$(() => {
+    initializeMap();
+})
